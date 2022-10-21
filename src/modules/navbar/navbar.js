03ce79c8aef5e8042c2a8cfb044aa60661ab7413
Maciej Dąbrowski <dabrowski.mac.1@gmail.com>
2022-10-21 17:31:52 +0200

    changed icon path

--- src/modules/navbar/navbar.js
+++ src/modules/navbar/navbar.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import arrowLeft from "../../assets/doodle-assets/arrow-left.png";
+import arrowLeft from "../../assets/doodle-assets/arrow-left-bold.png";
 import LogButtons from "../login-logout-buttons/login-out-buttons";
 import "./navbar.css";
 
