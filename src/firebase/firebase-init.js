7d85d00f482ad14812433e7cb66255efc5447313
Maciej Dąbrowski <dabrowski.mac.1@gmail.com>
2022-11-05 18:10:21 +0100

    changed firebase api key

--- src/firebase/firebase-init.js
+++ src/firebase/firebase-init.js
@@ -4,12 +4,12 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAes36UYuKN7iBSre4eQYym280J4cw_WFM",
-  authDomain: "where-s-waldo-3af39.firebaseapp.com",
-  projectId: "where-s-waldo-3af39",
-  storageBucket: "where-s-waldo-3af39.appspot.com",
-  messagingSenderId: "265369955077",
-  appId: "1:265369955077:web:764c13b62509baa31c256c",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 export const app = initializeApp(firebaseConfig);
